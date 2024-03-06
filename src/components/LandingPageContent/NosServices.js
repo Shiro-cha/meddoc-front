0fa6ec99c0176cd5a559b760b8207795dce95137
Nomena <noum.rzdr@gmail.com>
2024-03-06 14:23:50 +0100

    fix container

--- src/components/LandingPageContent/NosServices.js
+++ src/components/LandingPageContent/NosServices.js
@@ -26,7 +26,7 @@ export default function NosService() {
   };
 
   return (
-    <section className="container mx-auto py-8">
+    <section className="mx-auto py-8" style={{maxWidth: 1400}}>
       <h2 className="text-3xl font-bold mb-8 color-meddoc-blue title-meddoc">Nos Services</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
