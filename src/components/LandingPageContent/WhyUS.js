edc45a8ccef2be80690ae96de4b4e0b724e6c73e
Shiro Yami <shiro@localhost.localdomain>
2024-03-14 12:23:30 +0300

    update hero

--- src/components/LandingPageContent/WhyUS.js
+++ src/components/LandingPageContent/WhyUS.js
@@ -45,30 +45,13 @@ const items = [
 export default function Whyus() {
   return (
     <section id="why-us" class="why-us">
+       <h2 className="title-meddoc meddoc-blue">Simple, rapide et gratuit</h2>
       <div className="grid lg:grid-cols-4 gap-4 mx-4 my-5 lg:p-16 justify-center align-items-center">
-
-        <div className="flex flex-col p-4 items-center bg-cyan-400 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-96 dark:border-gray-700 dark:bg-gray-800 meddoc-blue">
-          <div class="content">
-            <h3 className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">Adoptons MEDDoC !</h3>
-            <p className="m-auto text-white font-normal font-medium text-lg text-justify">
-              Nous sommes convaincus que la télémédecine est un outil au service de l’amélioration de notre
-              système de santé au bénéfice de tous. C’est la simplicité, l’immédiateté et modifié la relation
-              soignant/soigné.
-            </p>
-            <button type="button" class="py-2.5 px-10 m-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">En savoir plus</button>
-
-          </div>
-
-
-        </div>
 
         {items.map((item) => (
           <div key={item.id} class="flex flex-col p-4 bg-white  border border-gray-200 rounded-lg shadow  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 " >
             <img src={item.icon} className="w-16 m-auto mb-4" alt="doctor_profil"></img>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">{item.desciption}</p>
-            {/*<Link to={item.link} class="mb-4 text-xl m-auto font-bold tracking-tight text-gray-900 dark:text-white">
-              <h5>{item.title}</h5>
-            </Link>*/}
           </div>
         ))}
 
