55d6264f4bc9895025964ef120752073ca471c9e
Shiro Yami <shiro@localhost.localdomain>
2024-03-14 12:52:28 +0300

    update hero

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,63 +1,76 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FaCalendarAlt, FaBook, FaMobileAlt, FaUserCog } from "react-icons/fa";
+'use client';
+import { DarkThemeToggle, Flowbite, Dropdown } from 'flowbite-react';
+import { Outlet, Link, useLocation } from "react-router-dom";
+import logo from "../../assets/image/logo.png";
+import { FaPhone, FaEnvelope, FaLinkedin } from 'react-icons/fa'; // Importing icons
 
-const items = [
-  {
-    id: 2,
-    link: "/e-learning",
-    icon: <FaCalendarAlt />,
-    description:
-      "Trouvez la disponibilité de tous nos praticiens en un coup d'œil.",
-  },
-  {
-    id: 3,
-    link: "/conseil-soignant",
-    icon: <FaBook />,
-    description:
-      "Réservez vos rendez-vous à tout moment, où que vous soyez, 24h/24 et 7j/7.",
-  },
-  {
-    id: 4,
-    link: "/conseil-soignant",
-    icon: <FaMobileAlt />,
-    description:
-      "Rappelez-vous tous vos rendez-vous grâce aux rappels SMS envoyés à l'avance.",
-  },
-  {
-    id: 5,
-    link: "/conseil-soignant",
-    icon: <FaUserCog />,
-    description:
-      "Gérez, déplacez ou annulez facilement vos rendez-vous.",
-  },
-];
+export default function Header() {
 
-export default function Whyus() {
+  const location = useLocation();
+
+  const menu_items = [
+    {
+      title_text: "Accueil",
+      link: "/",
+    },
+    {
+      title_text: "Nos Services",
+      link: "/nos-services",
+    },
+    
+  ]
+  const menu_button_items = [
+    {
+      title_text: "Se connecter",
+      link: "login",
+      className: "text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 meddoc-blue"
+    },
+    {
+      title_text: "Vous êtes practicients?",
+      link: "healthcare_sign_up",
+      className: "text-gray border border-blue-600  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:text-white focus:outline-none border-meddoc-blue",
+      new_blank:"_blank"
+    }
+  ]
+
   return (
-    <section id="why-us" className="why-us bg-orange-100">
-      <div className="container grid grid-cols-2 gap-4 lg:grid-cols-4 lg:p-16 justify-center items-center">
-        {items.map((item, index) => (
-          index !== 1 && (
-            <div
-              key={item.id}
-              className={`flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 ${
-                index > 1 ? "lg:mt-16" : ""
-              }`}
-            >
-              <Link to={item.link}>
-                <div className="w-16 h-16 bg-orange-300 rounded-full flex items-center justify-center">
-                  {item.icon}
-                </div>
-              </Link>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">
-                {item.description}
-              </p>
-            </div>
-          )
-        ))}
+    <header className='sticky top-0 z-[100] shadow'>
+      <div className="bg-gray-200 text-gray-600 py-2 px-4 flex items-center justify-center gap-3">
+        <div className="flex items-center space-x-2">
+          <FaPhone />
+          <a className="color-meddoc-dark" href="tel:123-456-7890">123-456-7890</a>
+        </div>
+        <div className="flex items-center space-x-2">
+          <FaEnvelope />
+          <a className="color-meddoc-dark" href="mailto:contact@meddoc.mg">contact@meddoc.mg</a>
+        </div>
+        <div className="flex items-center space-x-2">
+          <FaLinkedin />
+          <a href="https://www.linkedin.com" className="color-meddoc-dark">Meddoc</a>
+        </div>
       </div>
-    </section>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-4 dark:bg-gray-800 min-h-[68px]">
+        <div className="lg:flex justify-between items-center mx-auto ">
+          <Link className="mr-3 h-6 sm:h-9 flex items-center w-[160px] sm:max-w-[180px]" to='/'>
+            <img src={logo} alt='Medoc Logo'></img>
+            {location.pathname === '/healthcare_sign_up' && (
+              <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">PRO</span>
+            )}
+          </Link>
+          <div className="lg:flex hidden items-center lg:order-2">
+            {menu_items.map((menuItem, index) => (
+              <Link key={index} to={menuItem.link} className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"  >{menuItem.title_text}</Link>
+            ))}
+            {menu_button_items.map((menuItem, index) => (
+              <Link key={index} className={menuItem.className} to={menuItem.link} target={menuItem.new_blank}> {menuItem.title_text} </Link>
+            ))}
+            <Flowbite>
+              <DarkThemeToggle />
+            </Flowbite>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
 
--- src/components/LandingPageContent/WhyUS.js
+++ src/components/LandingPageContent/WhyUS.js
@@ -1,35 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import conseil from "../../assets/png//conseil-2.png";
-import rendezVous from "../../assets/png/rendez-vous -2.png";
-import eLearning from "../../assets/png/e-learning_2 (1).png";
+import { FaCalendarAlt, FaBook, FaMobileAlt, FaUserCog } from "react-icons/fa";
 
 const items = [
   {
     id: 2,
     link: "/e-learning",
-    icon: eLearning,
+    icon: <FaCalendarAlt />,
     description:
       "Trouvez la disponibilité de tous nos praticiens en un coup d'œil.",
   },
   {
     id: 3,
     link: "/conseil-soignant",
-    icon: conseil,
+    icon: <FaBook />,
     description:
       "Réservez vos rendez-vous à tout moment, où que vous soyez, 24h/24 et 7j/7.",
   },
   {
     id: 4,
     link: "/conseil-soignant",
-    icon: conseil,
+    icon: <FaMobileAlt />,
     description:
       "Rappelez-vous tous vos rendez-vous grâce aux rappels SMS envoyés à l'avance.",
   },
   {
     id: 5,
     link: "/conseil-soignant",
-    icon: conseil,
+    icon: <FaUserCog />,
     description:
       "Gérez, déplacez ou annulez facilement vos rendez-vous.",
   },
@@ -38,7 +36,6 @@ const items = [
 export default function Whyus() {
   return (
     <section id="why-us" className="why-us">
-        <h2 className="color-meddoc-blue title-meddoc">C'est simple, rapide et gratuit</h2>
       <div className="container grid grid-cols-2 gap-4 lg:grid-cols-4 lg:p-16 justify-center items-center">
         {items.map((item, index) => (
           index !== 1 && (
@@ -49,11 +46,9 @@ export default function Whyus() {
               }`}
             >
               <Link to={item.link}>
-                <img
-                  src={item.icon}
-                  className="w-16 m-auto mb-4 bg-orange-300 rounded-full"
-                  alt="doctor_profil"
-                />
+                <div className="w-16 h-16 bg-orange-300 rounded-full flex items-center justify-center">
+                  {item.icon}
+                </div>
               </Link>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">
                 {item.description}
