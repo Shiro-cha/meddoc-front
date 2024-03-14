0d8060fe126fbda7c8063e844a372e42da9aa355
Shiro Yami <shiro@localhost.localdomain>
2024-03-14 12:37:10 +0300

    update hero

--- src/components/LandingPageContent/Sponsoring.js
+++ src/components/LandingPageContent/Sponsoring.js
@@ -1,14 +1,9 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-
-
 import AAL from '../../assets/partenaires/aal.png';
 import AWS from '../../assets/partenaires/aws.png';
 import Orange from '../../assets/partenaires/orangefab.png';
@@ -16,58 +11,49 @@ import Zafitody from '../../assets/partenaires/zafitody.png';
 import incubeme from '../../assets/partenaires/incubeme.png';
 import usaid from '../../assets/partenaires/usaid.png';
 
-
-
 export default function Sponsoring() {
-const image_slides=[
-  AAL,
-  AWS,
-  Orange,
-  Zafitody,
-  incubeme,
-  usaid,
-]
+  const image_slides = [
+    [AAL, AWS, Orange, Zafitody],
+    [incubeme, usaid], // Second slide with remaining partners
+  ];
 
   return (
-
-   <section className='flex flex-col justify-center'>
-<div className='text-center text-4xl font-bold mb-10'>
-
-
-   <h2 className=''>Ils nous soutiennent </h2>
-</div>
-
-<div>
-<Swiper
-    spaceBetween={0}
-    centeredSlides={true}
-    autoplay={{
-      delay: 2500,
-      disableOnInteraction: false,
-    }}
-    pagination={{
-      clickable: true,
-    }}
-    navigation={true}
-    modules={[Autoplay, Pagination, Navigation]}
-    className="mySwiper lg:w-1/2"
-  >
-      {image_slides.map((image, index) => (
-        <SwiperSlide key={index} className="center-slide">
-          <img src={image} alt={`Slide ${index}`} className="center-image m-auto" />
-        </SwiperSlide>
-      ))}
-
-  </Swiper>
-</div>
-        
-
-
-</section>   
-
-  
-  
-  )
+    <section className="flex flex-col justify-center">
+      <div className="text-center text-4xl font-bold mb-10">
+        <h2>Ils nous soutiennent</h2>
+      </div>
+      <div>
+        <Swiper
+          spaceBetween={30} // Increase space between slides
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper lg:w-3/4" // Adjust width of Swiper
+        >
+          {image_slides.map((slide, index) => (
+            <SwiperSlide key={index}>
+              <div className="flex justify-center items-center">
+                {slide.map((partner, partnerIndex) => (
+                  <img
+                    key={partnerIndex}
+                    src={partner}
+                    alt={`Partner ${index}-${partnerIndex}`}
+                    className="m-4" // Add margin around each partner
+                  />
+                ))}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 }
-
 
--- src/components/LandingPageContent/WhyUS.js
+++ src/components/LandingPageContent/WhyUS.js
@@ -1,65 +1,67 @@
-import { Link } from "react-router-dom"
-import conseil from "../../assets/png//conseil-2.png"
-import rendezVous from "../../assets/png/rendez-vous -2.png"
-import eLearning from "../../assets/png/e-learning_2 (1).png"
+import React from "react";
+import { Link } from "react-router-dom";
+import conseil from "../../assets/png//conseil-2.png";
+import rendezVous from "../../assets/png/rendez-vous -2.png";
+import eLearning from "../../assets/png/e-learning_2 (1).png";
+
 const items = [
   {
-    id: 1,
-    title: 'Rendez-vous en ligne',
-    link: '/rendez-vous',
-    icon: rendezVous,
-    desciption:"Plateforme intuitive offrant des rendez-vous médicaux en ligne, simplifiant l'accès aux soins efficacement et rapidement"
-  },
-  {
     id: 2,
-    title: 'Hippocamp E-learning',
-    link: '/e-learning',
+    link: "/e-learning",
     icon: eLearning,
-    desciption:"Trouvez la disponibilité de tous nos praticiens en un coup d'œil."
+    description:
+      "Trouvez la disponibilité de tous nos praticiens en un coup d'œil.",
   },
   {
     id: 3,
-    title: 'Conseil santé',
-    link: '/conseil-soignant',
+    link: "/conseil-soignant",
     icon: conseil,
-    desciption:"Réservez vos rendez-vous à tout moment, où que vous soyez, 24h/24 et 7j/7."
+    description:
+      "Réservez vos rendez-vous à tout moment, où que vous soyez, 24h/24 et 7j/7.",
   },
   {
     id: 4,
-    title: 'Conseil santé',
-    link: '/conseil-soignant',
+    link: "/conseil-soignant",
     icon: conseil,
-    desciption:"Rappelez-vous tous vos rendez-vous grâce aux rappels SMS envoyés à l'avance."
+    description:
+      "Rappelez-vous tous vos rendez-vous grâce aux rappels SMS envoyés à l'avance.",
   },
   {
     id: 5,
-    title: 'Conseil santé',
-    link: '/conseil-soignant',
+    link: "/conseil-soignant",
     icon: conseil,
-    desciption:"Gérez, déplacez ou annulez facilement vos rendez-vous."
+    description:
+      "Gérez, déplacez ou annulez facilement vos rendez-vous.",
   },
-]
-
-
+];
 
 export default function Whyus() {
   return (
-    <section id="why-us" class="why-us">
-       <h2 className="title-meddoc meddoc-blue">Simple, rapide et gratuit</h2>
-      <div className="grid lg:grid-cols-4 gap-4 mx-4 my-5 lg:p-16 justify-center align-items-center">
-
-        {items.map((item) => (
-          <div key={item.id} class="flex flex-col p-4 bg-white  border border-gray-200 rounded-lg shadow  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 " >
-            <img src={item.icon} className="w-16 m-auto mb-4" alt="doctor_profil"></img>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">{item.desciption}</p>
-          </div>
+    <section id="why-us" className="why-us">
+      <div className="container grid grid-cols-2 gap-4 lg:grid-cols-4 lg:p-16 justify-center items-center">
+        {items.map((item, index) => (
+          index !== 1 && (
+            <div
+              key={item.id}
+              className={`flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 ${
+                index > 1 ? "lg:mt-16" : ""
+              }`}
+            >
+              <Link to={item.link}>
+                <img
+                  src={item.icon}
+                  className="w-16 m-auto mb-4 bg-orange-300 rounded-full"
+                  alt="doctor_profil"
+                />
+              </Link>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">
+                {item.description}
+              </p>
+            </div>
+          )
         ))}
-
-
-
       </div>
-
     </section>
-
-  )
+  );
 }
+
