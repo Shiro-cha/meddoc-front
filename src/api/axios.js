01bda24234796a00e632f193759ecfa253754bfc
Shiro Yami <shiro@localhost.localdomain>
2024-03-20 11:08:03 +0300

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,25 +1,23 @@
 import axios from 'axios';
 
-
 const BASE_URL = "http://16.16.212.56:8080";
 const BASE_URL_SEARCH = "http://16.16.212.56:9200";
 
+const axiosInstances = {
+  axiosSearch: axios.create({
+    baseURL: BASE_URL_SEARCH,
+    timeout: 5000,
+  }),
+  axiosPublic: axios.create({
+    baseURL: BASE_URL,
+    timeout: 5000,
+  }),
+  axiosPrivate: axios.create({
+    baseURL: BASE_URL,
+    timeout: 5000,
+    withCredentials: true,
+  }),
+};
 
-// Create Axios instances with the custom adapter
-export const axiosSearch = axios.create({
-  baseURL: BASE_URL_SEARCH,
-  timeout: 5000,
-
-});
-
-export const axiosPublic = axios.create({
-  baseURL: BASE_URL,
-  timeout: 5000,
-});
-
-export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
-  timeout: 5000,
-  withCredentials: true,
-});
+export default axiosInstances;
 
