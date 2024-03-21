770ca1a4c3479da23000b8a1e8a00d437eb6f12a
Shiro Yami <shiro@localhost.localdomain>
2024-03-21 15:45:27 +0300

    update

--- src/components/User/Appointment/TakeAppointment.js
+++ src/components/User/Appointment/TakeAppointment.js
@@ -298,7 +298,7 @@ export default function TakeAppointment() {
                                 <div className="p-2">
                                     <button type="button"
                                         onClick={handleOpen}
-                                        class=" p-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                        class=" p-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-orange-400 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                                         Selectionner le rendez-vous qui a été annuler par ce medecin:
                                     </button>
                                 </div>
