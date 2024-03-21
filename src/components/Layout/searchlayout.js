dbb97f3843f9a53ed1a48a40ebd88f63f4e149c8
Shiro Yami <shiro@localhost.localdomain>
2024-03-21 15:54:00 +0300

    update

--- src/components/Layout/searchlayout.js
+++ src/components/Layout/searchlayout.js
@@ -18,7 +18,7 @@ function SearchLayout() {
 
   return (
     <>
-      <section className="bg-cyan-700 text-white py-auto max-w-screen justify-start sticky top-[68px] z-[100]">
+      <section className="meddoc-dark text-white py-auto max-w-screen justify-start sticky top-[68px] z-[100]">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12 ">
           <div class="mr-auto place-self-center lg:col-span-7">
 
